7efed2fd858c7e2d6ada840d712891a266681f60
Bo Zhang <bo@gradle.com>
2019-06-02 09:32:33 +0800

    Initial commit

---
+++ src/test/java/com/github/hcsp/datatype/MainTest.java
@@ -0,0 +1,15 @@
+package com.github.hcsp.datatype;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+    @Test
+    public void test() {
+        assertEquals(false, Main.compare(123, 456));
+        assertEquals(true, Main.compare(123, 123));
+        assertEquals(false, Main.compare(123, null));
+        Main.class.getDeclaredMethod("compare", int.class, Integer.class);
+    }
+}
