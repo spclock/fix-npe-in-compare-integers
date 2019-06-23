5aebf3a97fd34254e1349fb1402c4691b3411ba9
Bo Zhang <bo@gradle.com>
2019-06-23 16:15:17 +0800

    Sync

--- src/test/java/com/github/hcsp/datatype/MainTest.java
+++ src/test/java/com/github/hcsp/datatype/MainTest.java
@@ -7,7 +7,7 @@ public class MainTest {
     @Test
     public void test() throws NoSuchMethodException {
         Assertions.assertEquals(false, Main.compare(123, 456));
-        Assertions.assertEquals(true, Main.compare(123, 123));
+        Assertions.assertEquals(true, Main.compare(1234, 1234));
         Assertions.assertEquals(false, Main.compare(123, null));
         Main.class.getDeclaredMethod("compare", int.class, Integer.class);
     }
