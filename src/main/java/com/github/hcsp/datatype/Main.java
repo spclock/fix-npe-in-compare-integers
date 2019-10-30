24892c1be95b64b9a4cfbd960de76dbbddeb01c8
Ditto0 <46198155+Ditto0@users.noreply.github.com>
2019-10-30 22:01:01 +0800

    1 (#96)
    1 (#96)

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -6,12 +6,17 @@ public class Main {
         System.out.println(compare(123, 456));
         System.out.println(compare(123, 123));
         System.out.println(compare(123, null));
+
     }
 
     // 比较一个int和一个Integer是否相等
     // 当且仅当它们代表的整数相等时，此方法返回true
     // 不要修改本方法参数的类型
     public static boolean compare(int a, Integer b) {
-        return a == b;
+        if(b==null||Integer.compare(a,b)!=0  ){
+            return false;
+        }
+        else
+            return true;
     }
 }
